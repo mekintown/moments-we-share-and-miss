338b26dc0826d81817a9d799503637c1f116b035
WorkingDada <6430222521@student.chula.ac.th>
2025-04-07 23:18:29 +0700

    add-text-size

--- src/app/(0-web-openning)/web-opening-1/page.tsx
+++ src/app/(0-web-openning)/web-opening-1/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-  return <></>;
+  return (
+    <div className="text-center text-subheader">
+      <div className="text-main-cream">
+        โปรเจกต์ที่มีจุดประสงค์เพื่อสำรวจ
+        ความสัมพันธ์และความทรงจำที่มีร่วมกับคนในครอบครัว 
+      </div>
+      <div className="text-main-yellow">
+        ระหว่าง ลูกในช่วงวัย 15–25 ปี กับ พ่อแม่หรือผู้ใหญ่ในบ้าน
+      </div>
+    </div>
+  );
 }
