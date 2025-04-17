7041f638c708881ae4ef72eb63ec1da8bdf192be
WorkingDada <6430222521@student.chula.ac.th>
2025-04-17 21:23:05 +0700

    feat: add reveal

--- src/app/(6-web-reveal)/web-reveal-2/page.tsx
+++ src/app/(6-web-reveal)/web-reveal-2/page.tsx
@@ -1,19 +1,32 @@
 "use client";
 
 import NextButton from "@/components/NextButton";
+import Image from "next/image";
 import { useState, useEffect } from "react";
 
 const Page = () => {
   const [answerImportant, setAnswerImportant] = useState<string | null>(null);
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
   useEffect(() => {
     const savedImportant = localStorage.getItem("web_answer_important");
+    const savedImgSrc = localStorage.getItem("imageSrc");
+    setImageSrc(savedImgSrc);
     setAnswerImportant(savedImportant);
   }, []);
 
   return (
     <>
       <div className="row-span-3 text-center items-center text-subheader text-main-cream">
+        {imageSrc && (
+          <Image
+            src={imageSrc}
+            alt="reveal-image"
+            width={300}
+            height={300}
+            className="mx-auto py-5"
+          />
+        )}
         <div className="">{answerImportant}</div>
       </div>
       <div className="row-start-4 ">
