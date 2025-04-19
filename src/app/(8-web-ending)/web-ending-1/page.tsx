81ba0a0a82ca075305328240941af2d03fe865d0
Maekin <74455990+mekintown@users.noreply.github.com>
2025-04-19 19:58:47 +0700

    add letter page

--- src/app/(8-web-ending)/web-ending-1/page.tsx
+++ src/app/(8-web-ending)/web-ending-1/page.tsx
@@ -3,14 +3,11 @@ import NextButton from "@/components/NextButton";
 const Page = () => {
   return (
     <>
-      <div className="row-start-2 row-span-2 text-center items-center text-subheader text-main-cream">
-        <div className="">เขาจะเลือกเรื่องราวไหนกันนะ</div>
-      </div>
       <div className="row-start-4 ">
         <NextButton
           variant="ghost"
-          label="แตะเพื่อไปต่อ"
-          url="/web-ending-1"
+          label="แตะหน้าจอเพื่อเปิด"
+          url="/web-letter"
           className="text-remark text-main-cream opacity-60 hover:opacity-100 hover:text-main-cream hover:bg-transparent"
         />
       </div>
