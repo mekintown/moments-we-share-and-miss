9da8536f684c831e5e991b97de1b73fb4665556b
Maekin Namwong <74455990+mekintown@users.noreply.github.com>
2025-04-22 21:57:12 +0700

    Migrate Image to Cloud (#25)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
+const assetPaths = [
+  "animations",
+  "audio",
+  "backgrounds",
+  "illustrations",
+  "logo",
+  "memorycards",
+];
+
+const BASE_MEDIA_URL = "https://media.themomentsweshareandmiss.com";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        hostname: "media.themomentsweshareandmiss.com",
+      },
+    ],
+  },
+  async redirects() {
+    return assetPaths.map((path) => ({
+      source: `/${path}/:path*`,
+      destination: `${BASE_MEDIA_URL}/${path}/:path*`,
+      permanent: true,
+    }));
+  },
 };
 
 export default nextConfig;
