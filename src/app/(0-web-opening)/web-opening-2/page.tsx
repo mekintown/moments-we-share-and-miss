8869c5afbba01348e4fa3ee06dc13b67c1a6ec2f
WorkingDada <6430222521@student.chula.ac.th>
2025-04-24 02:03:38 +0700

    fix: add overflow

--- src/app/(0-web-opening)/web-opening-2/page.tsx
+++ src/app/(0-web-opening)/web-opening-2/page.tsx
@@ -1,40 +1,50 @@
 "use client";
 import NextButton from "@/components/NextButton";
 import { Separator } from "@/components/ui/separator";
-import { motion } from "framer-motion";
-
+import Image from "next/image";
 export default function Page() {
   return (
-    <div className="text-center text-body space-y-2">
-      <Separator className="bg-main-yellow" />
-      <div className="text-main-cream">
-        <div className="">เราจึงอยากขอชวนคุณใช้เวลาประมาณ</div>
-        <div className="">7 นาทีค่อย ๆ ย้อนกลับไปใน</div>
-        <div className="">ห้วงความทรงจำ นึกถึงวันวลาที่เคยได้</div>
-        <div className="">แบ่งปันรอยยิ้ม น้ำตา หรือเสียงหัวเราะ</div>
-        <div className="">ร่วมกับคนในครอบครัว</div>
-        <div className="">หากให้นึกถึงช่วงเวลาเหล่านั้น</div>
-        <div className="">มีเรื่องไหนที่เรายังจำได้อยู่เสมอบ้างนะ…</div>
-      </div>
-      <Separator className="bg-main-yellow" />
-
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="text-main-cream z-10 text-nowrap"
-      >
-        <div className="">'Within That Moment'</div>
-        <div className="">เป็นส่วนหนึ่งของการศึกษาปริญญานิพนธ์</div>
-        <div className="">คณะสถาปัตยกรรมศาสตร์</div>
-        <div className="">สาขาการออกแบบนิเทศศิลป์ จุฬาลงกรณ์มหาวิทยาลัย</div>
-        <NextButton
-          variant="link"
-          label="เกี่ยวกับโปรเจกต์"
-          url="/web-opening-3"
-          className="!text-body"
+    <>
+      <div className="row-start-1">
+        <Image
+          src="/logo/logo-full.svg"
+          alt="logo-full"
+          width={126}
+          height={78}
         />
-      </motion.div>
-    </div>
+      </div>
+      <div className="row-span-2 row-start-2 overflow-auto">
+        <div className="text-body space-y-2">
+          <Separator className="bg-main-yellow" />
+          <div className="text-main-cream text-center">
+            <div className="">เราจึงอยากขอชวนคุณใช้เวลาประมาณ</div>
+            <div className="">7 นาทีค่อย ๆ ย้อนกลับไปใน</div>
+            <div className="">ห้วงความทรงจำ นึกถึงวันเวลาที่เคยได้</div>
+            <div className="">แบ่งปันรอยยิ้ม น้ำตา หรือเสียงหัวเราะ</div>
+            <div className="">ร่วมกับคนในครอบครัว</div>
+            <div className="">หากให้นึกถึงช่วงเวลาเหล่านั้น</div>
+            <div className="">มีเรื่องไหนที่เรายังจำได้อยู่เสมอบ้างนะ…</div>
+          </div>
+          <Separator className="bg-main-yellow " />
+
+          <div className="text-main-cream text-nowrap text-center">
+            <div className="">พื้นที่แห่งนี้เป็นส่วนหนึ่งของ</div>
+            <div className="">การศึกษาปริญญานิพนธ์ คณะสถาปัตยกรรมศาสตร์</div>
+            <div className="">
+              สาขาการออกแบบนิเทศศิลป์ จุฬาลงกรณ์มหาวิทยาลัย
+            </div>
+            <NextButton
+              variant="link"
+              label="เกี่ยวกับโปรเจกต์"
+              url="/web-opening-3"
+              className="!text-body underline"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="row-start-4">
+        <NextButton url="/web-opening-4" />
+      </div>
+    </>
   );
 }
--- src/app/(0-web-opening)/web-opening-4/page.tsx
+++ src/app/(0-web-opening)/web-opening-4/page.tsx
@@ -1,37 +1,55 @@
 "use client";
 
+import NextButton from "@/components/NextButton";
 import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="row-span-2 text-center text-body space-y-2">
-      <div className="text-subheader text-main-cream">สำหรับผู้ใช้งาน IOS</div>
-      <Separator className="bg-main-yellow" />
-      <div className="text-body-bold text-main-cream">
-        เพื่อประสบการณ์ที่สมบูรณ์
-      </div>
-      <div className="text-main-cream text-body-bold">
-        แนะนำให้ใช้งานผ่าน <span className="text-main-yellow">SAFARI</span>
-      </div>
-      <div className="text-main-cream text-body-bold">
-        และ<span className="text-main-yellow">ซ่อนแถบเครื่องมือ</span>
-      </div>
-      <Separator className="bg-main-yellow" />
-      <div className="gap-3 flex flex-col items-center">
+    <>
+      <div className="row-start-1">
         <Image
-          src="/illustrations/ios1.webp"
-          alt="ios-1"
-          height={100}
-          width={210}
-        />
-        <Image
-          src="/illustrations/ios2.webp"
-          alt="ios-2"
-          height={200}
-          width={150}
+          src="/logo/logo-full.svg"
+          alt="logo-full"
+          width={126}
+          height={78}
         />
       </div>
-    </div>
+      <div className="row-start-2 row-span-2 h-full overflow-auto">
+        <div className="text-center text-body space-y-2">
+          <div className="text-subheader text-main-cream">
+            สำหรับผู้ใช้งาน IOS
+          </div>
+          <Separator className="bg-main-yellow" />
+          <div className="text-body-bold text-main-cream">
+            เพื่อประสบการณ์ที่สมบูรณ์
+          </div>
+          <div className="text-main-cream text-body-bold">
+            แนะนำให้ใช้งานผ่าน <span className="text-main-yellow">SAFARI</span>
+          </div>
+          <div className="text-main-cream text-body-bold">
+            และ<span className="text-main-yellow">ซ่อนแถบเครื่องมือ</span>
+          </div>
+          <Separator className="bg-main-yellow" />
+          <div className="gap-3 flex flex-col items-center">
+            <Image
+              src="/illustrations/ios1.webp"
+              alt="ios-1"
+              height={100}
+              width={210}
+            />
+            <Image
+              src="/illustrations/ios2.webp"
+              alt="ios-2"
+              height={200}
+              width={150}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="row-start-4">
+        <NextButton url="/web-opening-5" />
+      </div>
+    </>
   );
 }
