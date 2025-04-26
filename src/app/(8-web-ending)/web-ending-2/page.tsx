1312e9a687fa2b94790e432d443c9025a22c8ea8
Maekin <74455990+mekintown@users.noreply.github.com>
2025-04-27 02:21:15 +0700

    fix

--- src/app/(8-web-ending)/web-ending-2/page.tsx
+++ src/app/(8-web-ending)/web-ending-2/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { Download } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import NextButton from "@/components/NextButton";
